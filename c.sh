d3a27dcb00a8560c046de6d5be6d81ab2c5b6407
Eirik Nydal <nydal91@users.noreply.github.com>
2024-09-27 11:23:26 +0200

    Add files via upload

---
+++ c.sh
@@ -0,0 +1,24 @@
+cd ~/.wine/drive_c/users/root/Documents/
+cp /opt/Resources/f5taskmgr.exe
+ ~/.wine/drive_c/windows
+tar -xvf /sdcard/download/dxvk-2.4.tar.gz -C ~/.wine/drive_c/
+tar -xvf /sdcard/download/dxvk-gplasync-v2.4-1.tar.gz -C ~/.wine/drive_c/
+
+dx12
+tar -xvf /sdcard/download/vkd3d-proton-2.13.tar.zst -C ~/.wine/drive_c/
+tar -xvf /sdcard/download/dxvk-async-1.10.3.tar.gz -C ~/.wine/drive_c/
+tar -xvf /sdcard/download/wine-9.17-staging-tkg-amd64-wow64.tar.xz -C /opt/
+tar -xvf /sdcard/download/wine-proton-exp-9.0-68775b318a8-exp-wow64-amd64.tar.xz -C /opt/
+
+###ingen error trur e
+
+tar -xvf /sdcard/download/dxvk-gplasync-v2.2-4.tar.gz -C ~/.wine/drive_c/
+tar -xvf /sdcard/download/mesa-24.3.0-turnip-290724.tar.xz
+
+##ca 15-20 min med ingen feil vindu mode unity 40 fps
+## delete SLT.dxvk-cache
+tar -xvf /sdcard/download/dxvk-1.10.3.tar.gz -C ~/.wine/drive_c/
+
+## test 100
+tar -xvf /sdcard/download/dxvk-2.2.tar.gz -C ~/.wine/drive_c/
+export GST_PLUGIN_PATH=/usr/lib/aarch64-linux-gnu/gstreamer-1.0
